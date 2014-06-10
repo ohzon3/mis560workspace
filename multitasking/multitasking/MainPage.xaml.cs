5e0fea6c57b1774c925b458ff446f901edb91d42
ohzon3 <kevinoh85@gmail.com>
2014-06-09 19:39:48 -0500

    week7inclass added background agent code

--- multitasking/multitasking/MainPage.xaml.cs
+++ multitasking/multitasking/MainPage.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using BackgroundAgent;
+using Microsoft.Phone.Scheduler;
+
 
 namespace multitasking
 {
@@ -19,6 +22,20 @@ namespace multitasking
         public MainPage()
         {
             InitializeComponent();
+
+            // A unique name for your task. It is used to 
+            // locate it in from the service. 
+            var taskName = "MyTask";
+
+            // Create the Task 
+            PeriodicTask task = new PeriodicTask(taskName);
+
+            // Description is required 
+            task.Description = "This saves some data to Isolated Storage";
+
+            // Add it to the service to execute ScheduledActionService.Add(task); 
+
+
         }
     }
 }
