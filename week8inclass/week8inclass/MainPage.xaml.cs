799bfd151929324b3e3af96edaa7df37c319e2ae
ohzon3 <kevinoh85@gmail.com>
2014-06-16 19:15:33 -0500

    week8inclass accel working

--- week8inclass/week8inclass/MainPage.xaml.cs
+++ week8inclass/week8inclass/MainPage.xaml.cs
@@ -11,21 +11,55 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
 using Microsoft.Devices;
-
+using Microsoft.Devices.Sensors;
 
 namespace week8inclass
 {
     public partial class MainPage : PhoneApplicationPage
     {
-        // Constructor
+        //Constructor
         public MainPage()
         {
             InitializeComponent();
+            _theAccelerometer.CurrentValueChanged += _theAccelerometer_CurrentValueChanged;
+
+            //determine how often you want updates 
+            _theAccelerometer.TimeBetweenUpdates = TimeSpan.FromMilliseconds(50);
+
         }
 
         private void vibrate_Click(object sender, RoutedEventArgs e)
         {
             VibrateController.Default.Start(TimeSpan.FromMilliseconds(10));
         }
+
+        Accelerometer _theAccelerometer = new Accelerometer();
+
+
+
+        void _theAccelerometer_CurrentValueChanged(object sender, SensorReadingEventArgs<AccelerometerReading> e)
+        {
+            var position = e.SensorReading.Acceleration;
+
+            // Update the User Interface 
+            Dispatcher.BeginInvoke(() =>
+            {
+                xValue.Text = "xValue = " + position.X.ToString("0.00");
+                yValue.Text = "yValue = " + position.Y.ToString("0.00");
+                zValue.Text = "zValue = " + position.Z.ToString("0.00");
+
+            });
+        } 
+
+        private void accelStart_Click(object sender, RoutedEventArgs e)
+        {
+            _theAccelerometer.Start(); 
+        }
+
+        private void accelStop_Click(object sender, RoutedEventArgs e)
+        {
+            _theAccelerometer.Stop(); 
+        } 
+
     }
 }
